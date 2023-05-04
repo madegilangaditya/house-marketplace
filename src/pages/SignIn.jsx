9da8558605a1cfaba0c6f2db74e641ea00baa09f
gilang aditya <madegilangaditya32@gmail.com>
2023-05-04 11:29:07 +0800

    add sign in and sign up auth

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,5 +1,6 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth"
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -21,6 +22,21 @@ function SignIn() {
     }))
   }
 
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+      const userCredential = await signInWithEmailAndPassword(auth, email, password)
+      
+      if(userCredential.user){
+        navigate('/')
+      }
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -29,7 +45,7 @@ function SignIn() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input id="email" type="email" className="emailInput" placeholder="Email" value={email} onChange={onChange} />
             <div className="passwordInputDiv">
               <input type={showPassword ? 'text' : 'password'} className="passwordInput" id="password" value={password} placeholder="Password" onChange={onChange} />
--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -1,5 +1,8 @@
 import { useState } from "react"
 import { Link, useNavigate } from "react-router-dom"
+import {getAuth, createUserWithEmailAndPassword, updateProfile} from 'firebase/auth'
+import { setDoc, doc, serverTimestamp } from "firebase/firestore"
+import {db} from '../firebase.config'
 import {ReactComponent as ArrowRightIcon} from '../assets/svg/keyboardArrowRightIcon.svg'
 import visibilityIcon from '../assets/svg/visibilityIcon.svg'
 
@@ -22,6 +25,31 @@ function SignUp() {
     }))
   }
 
+  const onSubmit = async (e) =>{
+    e.preventDefault()
+
+    try {
+      const auth = getAuth()
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password)
+
+      const user = userCredential.user
+
+      updateProfile(auth.currentUser, {
+        displayName: name,
+      })
+
+      const formDataCopy = {...formData}
+      delete formDataCopy.password
+      formDataCopy.timeStamp = serverTimestamp()
+
+      await setDoc(doc(db, 'users', user.uid), formDataCopy)
+
+      navigate('/')
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
   return (
     <>
       <div className="pageContainer">
@@ -30,7 +58,7 @@ function SignUp() {
         </header>
 
         <main>
-          <form>
+          <form onSubmit={onSubmit}>
             <input id="name" type="text" className="nameInput" placeholder="Name" value={name} onChange={onChange} />
             <input id="email" type="email" className="emailInput" placeholder="Email" value={email} onChange={onChange} />
             <div className="passwordInputDiv">
